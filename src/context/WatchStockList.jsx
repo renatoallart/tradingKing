552b5f7fc919c477bc981b71d346f0b10488f1ea
Renato Santos <renato.senstec@gmail.com>
2023-01-12 15:22:00 -0300

    update

--- src/context/WatchStockList.jsx
+++ src/context/WatchStockList.jsx
@@ -1,29 +1,34 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useState, useEffect } from "react";
 
-const WatchListContext = createContext()
+const WatchListContext = createContext();
 
 export function useWatchListContext() {
-  return useContext(WatchListContext)
+  return useContext(WatchListContext);
 }
 
 export function WatchStockList({ children }) {
+  const [watchList, setWatchList] = useState(
+    localStorage.getItem("stock")?.split(",") || ["GOOGL", "MSFT", "AMZN"]
+  );
 
-  const [watchList, setWatchList] = useState(['GOOGL', 'MSFT', 'AMZN'])
+  useEffect(() => {
+    localStorage.setItem("stock", watchList);
+  }, [watchList]);
 
   function addStock(stock) {
-    if (watchList.includes(stock)) return
-    return setWatchList([...watchList, stock])
+    if (watchList.includes(stock)) return;
+    return setWatchList([...watchList, stock]);
   }
 
   function deleteStock(stockSymbol) {
-    setWatchList(oldStock =>
-      oldStock.filter(symbol => symbol !== stockSymbol))
+    setWatchList((oldStock) =>
+      oldStock.filter((symbol) => symbol !== stockSymbol)
+    );
   }
-
 
   return (
     <WatchListContext.Provider value={{ watchList, addStock, deleteStock }}>
       {children}
     </WatchListContext.Provider>
-  )
+  );
 }

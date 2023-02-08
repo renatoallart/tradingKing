79155cf3185ad053768a15725926fa6492ae8deb
José Renato Santana Santos <renato.senstec@gmail.com>
2023-02-08 12:24:37 -0300

    test

--- src/apis/finnHub.js
+++ src/apis/finnHub.js
@@ -1,8 +1,9 @@
 import axios from "axios"
 
+const token = import.meta.env.VITE_FINNHUB_TOKEN
 export const finnHub = axios.create({
   baseURL: "https://finnhub.io/api/v1",
   params: {
-    token: import.meta.env.PROD.VITE_FINNHUB_TOKEN,
+    token: token,
   },
 })

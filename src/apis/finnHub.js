b74cd46841e0f7443ae7cf97d6a64f06bd4870ce
José Renato Santana Santos <renato.senstec@gmail.com>
2023-02-08 12:14:59 -0300

    .env.production: create a environment variable to finnhub token for production

--- src/apis/finnHub.js
+++ src/apis/finnHub.js
@@ -3,6 +3,6 @@ import axios from "axios"
 export const finnHub = axios.create({
   baseURL: "https://finnhub.io/api/v1",
   params: {
-    token: import.meta.env.PROD,
+    token: import.meta.env.PROD.VITE_FINNHUB_TOKEN,
   },
 })
